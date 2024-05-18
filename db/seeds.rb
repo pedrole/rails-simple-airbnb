0a39e34511ea20506dc7741629ed2d8a07a054f4
Pedro Leal <lealpedro2009@gmail.com>
2024-05-18 17:30:45 +0100

    Solved Rails Simple Airbnb

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,33 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Flat.destroy_all if Rails.env.development?
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+Flat.create!(
+  name: 'Dark & Spacious Garden Flat Lisbon',
+  address: '20 Clifton Gardens London W9 1DT',
+  description: 'A lovely winter feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 55,
+  number_of_guests: 2
+)
+Flat.create!(
+  name: 'Light & Narrow Garden Flat Barcelona',
+  address: '30 Clifton Gardens Barcelona W9 1DT',
+  description: 'A lovely automn feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 76,
+  number_of_guests: 6
+)
+Flat.create!(
+  name: 'Light & Spacious Garden Flat Madrid',
+  address: '60 Clifton Gardens Madrid W9 1DT',
+  description: 'A lovely spring feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 70,
+  number_of_guests: 9
+)
